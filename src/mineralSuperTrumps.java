5364df7c5b08497bbbb0ad00f698bfc1062ee91f
CharlieSchot <charlie.schot@my.jcu.edu.au>
2016-08-28 21:35:35 +1000

    Added a basic menu to start off the project, welcoming the player to the game and asking them how many players are playing

---
+++ src/mineralSuperTrumps.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by CharlieSchot on 28/08/2016.
+ */
+public class mineralSuperTrumps {
+    public static void main(String[] args) {
+        int playerAmount;
+        System.out.println("Welcome to Mineral Super Trumps!");
+        Scanner reader = new Scanner(System.in);
+        System.out.println("How many players will be playing? \n 3 Players \n 4 Players \n 5 Players");
+        playerAmount = reader.nextInt();
+
+    }
+
+    }
+
